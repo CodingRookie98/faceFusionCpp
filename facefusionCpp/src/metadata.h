cdd00ffefc01543318ba3eb9697b6a65311aaed4
CodingRookie98 <520whjoker@gmail.com>
2024-08-13 22:58:35 +0800

    0.2.1

--- facefusionCpp/src/metadata.h
+++ facefusionCpp/src/metadata.h
@@ -17,7 +17,7 @@ namespace Ffc::MetaData {
 
 const std::string name = "FaceFusionCPP";
 const std::string description = "This project is a C++ implementation of the open-source project facefusion.";
-const std::string version = "0.2.0";
+const std::string version = "0.2.1";
 const std::string license = "GPL-3.0 License";
 const std::string author = "CodingRookie";
 const std::string url = "https://github.com/CodingRookie98/faceFusionCpp";
